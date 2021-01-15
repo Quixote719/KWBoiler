1dfac45c98dcf840dc93c38914cf742e7682f42b
LAPTOP-URN1R8TL\lwang <dwang@dsny.nyc.gov>
2021-01-15 22:19:20 +0800

    update gulp, fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -22,45 +22,66 @@ const PACKAGE_JSON = {
     output: path.resolve(__dirname, 'build')
 }
 
-gulp.task('copy-all', [
-    'copy-server',
-    'copy-resource',
-    'copy-template',
-    'copy-packageJson'
-])
+// gulp.task('copy-all', [
+// 'copy-server',
+// 'copy-resource',
+// 'copy-template',
+// 'copy-packageJson'
+// ])
 
 gulp.task('clean-build', function(cb) {
-    del([__dirname + '/build/**/*'], cb)
-})
-
-//监视服务器文件变化
-gulp.task('watch-server', ['copy-server'], function() {
-    gulp.watch(SERVER_PATHS.watchFiles, ['copy-server'])
+    return new Promise(resolve => {
+        del([__dirname + '/build/**/*'], cb)
+        resolve()
+    })
 })
 
 //拷贝服务器文件
-gulp.task('copy-server', function() {
+gulp.task('copy-server', () => {
     return gulp.src(SERVER_PATHS.files).pipe(gulp.dest(SERVER_PATHS.output))
 })
+//监视服务器文件变化
+gulp.task(
+    'watch-server',
+    gulp.series('copy-server', () => {
+        gulp.watch(SERVER_PATHS.watchFiles, ['copy-server'])
+    })
+)
 
 //将res拷贝到build/www中
 gulp.task('copy-resource', function() {
-    gulp.src(STATIC_RES.files).pipe(gulp.dest(STATIC_RES.output))
+    return gulp.src(STATIC_RES.files).pipe(gulp.dest(STATIC_RES.output))
 })
 
 //将res和index.html拷贝到build/www中
 gulp.task('copy-template', function() {
-    gulp.src(__dirname + '/index.html')
+    return gulp
+        .src(__dirname + '/index.html')
         .pipe(rename('__index__template__.html'))
         .pipe(gulp.dest(__dirname + '/build/www'))
 })
 
 //将res和index.html拷贝到build/www中
 gulp.task('copy-packageJson', function() {
-    gulp.src(PACKAGE_JSON.files).pipe(gulp.dest(PACKAGE_JSON.output))
+    return gulp.src(PACKAGE_JSON.files).pipe(gulp.dest(PACKAGE_JSON.output))
 })
 
 gulp.task(
     'git:diff',
     shell.task('git diff --name-status --cached HEAD > script/git_diff.log')
+)
+
+gulp.task(
+    'copy-all',
+    gulp.series(
+        'copy-server',
+        'copy-resource',
+        'copy-template',
+        'copy-packageJson',
+        () => {
+            return new Promise(resolve => {
+                resolve()
+            })
+        }
+    )
 )
